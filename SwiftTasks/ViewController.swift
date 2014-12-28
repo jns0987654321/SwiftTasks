e479a9bfdb1d27c7dc7e73e12a361ed48fc2d9b3
Junior Samaroo <jns0987654321@gmail.com>
2014-12-28 13:48:39 -0400

    Initial Commit

---
+++ SwiftTasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTasks
+//
+//  Created by Junior Samaroo on 2014-12-28.
+//  Copyright (c) 2014 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
